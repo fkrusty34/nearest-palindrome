47d645ecbd00b85cc988517a0d54e5031dd9d818
fkrusty34 <yarik@shanin.spb.ru>
2021-07-16 12:29:47 +0300

    program

---
+++ palindrome.py
@@ -0,0 +1,30 @@
+#starting values of variables
+x = int(input ())
+a = 0
+b = 0
+c = False
+
+#if the number is negative
+if x < 0:
+x = str(x) [1:]
+x = int(x)
+c = True
+
+#algorithm for finding the nearest palindrome
+while True:
+ b = x
+ b = x + a
+ if str(b) == str(b)[::-1]:
+ break
+ b = x - a
+ if str(b) == str(b)[::-1]:
+ break
+ a = a + 1
+
+#if the number is negative
+if c:
+b = " - " + str (b)
+b = int (b)
+
+#output
+print(b)
